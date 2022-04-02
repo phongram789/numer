582f26fb0a9c86e7fd06e54420e192d62a526c51
พงศ์ราม พึ่งบุญ ณ อยุธยา <73733731+phongram789@users.noreply.github.com>
2022-04-02 20:00:28 +0700

    UPDATE

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,29 @@
 import axios from "axios";
 import React from "react";
-const baseURL = "http://localhost:5000/api/Bisection";
+const baseURL = "http://localhost:5000/api/Onepoint";
+
+function cal(x){
+  
+  let result = (1/2)-x;
+  return result;
+}
+
+function One_pst(X){
+  let X1 = X;
+  let Xs = cal(X1);
+for(var i = 0 ; i<10000;i++){
+  if((X1-Xs)/X1 > 0.000001){
+    return X1
+    
+  }
+  else{
+          X1 = Xs;
+          Xs = cal(X1);
+
+  }
+}
+}
+
 
 export default function App() {
   const [post, setPost] = React.useState(null);
@@ -10,17 +33,16 @@ export default function App() {
       setPost(response.data);
     });
   }, []);
-
   if (!post) return null;
 
-  let X = post.xR
-  let cal = X*X
+  var X = post.x0;
 
   return (
     <div>
       <h1>{post.id}</h1>
-      <h1>{cal}</h1>
-      
+      <h1>{X}</h1>
+      <h1>{One_pst(X)}</h1>
     </div>
   );
 }
+
