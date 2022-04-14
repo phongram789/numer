211ddad73c74b2d03b55422019a97d3de076043e
พงศ์ราม พึ่งบุญ ณ อยุธยา <73733731+phongram789@users.noreply.github.com>
2022-04-14 16:21:17 +0700

    update
    react-router-dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,49 @@
-/*import { BrowserRouter as Link,Router } from 'react-router-dom'*/
-import Bar from './componants/barmenu';
-import Bit from './rootofequetion/Bitsection';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import Bitsection from './rootofequetion/Bitsection';
 import { Menu, Dropdown, Button } from 'antd';
-
-
-import * as React from 'react';
-import './App.css';
 import 'antd/dist/antd.css';
-
+import './App.css';
 import {Layout} from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+
+
 const { Content , Header} = Layout;
+
+const menu1 = (
+  <Menu>
+    <Menu.Item >
+      <Link to="/Bitsection">Bitsection</Link>
+    </Menu.Item>
+    <Menu.Item >
+      <Link to="/Falsposition">Falsposition</Link>
+    </Menu.Item>
+  </Menu>
+);
 
 function App(){
   return(
-    <div className = "App">
+    <Router>
       <Layout>
-      <Bar/>
-      <Content>
-      <Bit/>
-      </Content>
-    </Layout>
-    
-    </div>
+        <Header>
+          <div className="logo"  />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Dropdown overlay={menu1}>
+                <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                Root of Equation <DownOutlined />
+                </a>
+              </Dropdown>
+          </Menu>
+        </Header>
+
+        <Content className="site-layout-background" style={{ padding: 24,margin: 0,minHeight: 600, }}>
+          <Routes>
+            <Route exact path="/Bitsection" element={<Bitsection/>}/>
+          </Routes>
+        </Content>
+
+      </Layout>
+    </Router>
   )
 }
 export default App;
