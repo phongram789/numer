fde529786efb7bd14b1ae636570c14a28e408e98
พงศ์ราม พึ่งบุญ ณ อยุธยา <73733731+phongram789@users.noreply.github.com>
2022-05-25 17:12:09 +0700

    swagger basic activate

--- api/server.js
+++ api/server.js
@@ -3,11 +3,13 @@ const auth = require('json-server-auth')
 const middlewares = jsonServer.defaults({noCors:true})
 const app = jsonServer.create()
 const router = jsonServer.router('data.json')
+const swaggerUI=require('swagger-ui-express')
+const swaggerDocumant = require('./swag.json')
 var cors = require("cors")
+
 const rules = auth.rewriter({
   users:660,
   api:660,
-
 })
 
 // /!\ Bind the router db to the app
@@ -19,6 +21,7 @@ app.use(cors())
 app.use(rules)
 app.use(auth)
 app.use(middlewares)
+app.use('/swagger',swaggerUI.serve,swaggerUI.setup(swaggerDocumant))
 app.use(router)
 app.listen(5000, () => {
   console.log('JSON Server with Token is running')
